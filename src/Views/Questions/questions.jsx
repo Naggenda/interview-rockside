cf489db8f675a83ef0083b0ef0772a535657883e
Naggenda <naggendajoshua@gmail.com>
2022-11-10 16:23:12 +0300

    initial project setup

---
+++ src/Views/Questions/questions.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NotListedLocationIcon from '@mui/icons-material/NotListedLocation';
+
+const questions = () => {
+    return (
+        <div>
+            <div className="title">
+            <h4>Questions <NotListedLocationIcon/></h4>
+            </div>
+        </div>
+    )
+}
+
+export default questions
